654a8af742610c6bcc96a4d016d529a537ff6b86
kazimdfoisal <kazimdfoisal7409@gmail.com>
2023-04-07 15:05:27 +0600

    chage
    Co-authored-by: Sefat Anam <sefatanam@users.noreply.github.com>

--- script/app.js
+++ script/app.js
@@ -1,62 +1,102 @@
 import { threeInputs } from "./array.js";
 import { inBirthData } from "./functions.js";
-import { age} from "./document.js";
+import { age } from "./document.js";
 
-
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     for (const data of threeInputs) {
-        dataEntry.innerHTML+=inBirthData(data);
+        document.getElementById("dataEntry").innerHTML += inBirthData(data);
     }
-    document.getElementById("ageShow").innerHTML="_" + " Years "+"<br>"+ "_" + " Months " +"<br>"+ "_" + " Days";
-})
+    document.getElementById("ageShow").innerHTML = "_" + " Years " + "<br>" + "_" + " Months " + "<br>" + "_" + " Days";
+});
 
 
- age.addEventListener("click", ()=> 
- {
-    var getDayValue = Number(document.getElementById("days").value);
-    var getMonthValue = Number(document.getElementById("months").value);
-    var getYearValue = Number(document.getElementById("years").value);
-    var msg = document.getElementById("msg");
+function validateBirthDate({
+    day,
+    month,
+    year
+}) {
+    var messageLabelEl = document.getElementById("msg");
 
-    var date = new Date();
-    var currDay = date.getDate();
-    var currMonth = 1 + date.getMonth();
-    var currYear = date.getFullYear();
-    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if (getDayValue > currDay) {
-        currDay = currDay + month[currMonth - 1];
-        currMonth = currMonth - 1;
+    /* TODO: Move logic to switch case */
+    if (day == "" || month == "" || year == "") {
+        return false;
     }
-    if (getMonthValue > currMonth) {
-        currMonth = currMonth + 12;
-        currYear = currYear - 1;
-    }
-    var days = currDay - getDayValue;
-    var months = currMonth - getMonthValue;
-    var years = currYear - getYearValue;
-
-    if (getDayValue==""||getMonthValue==""||getYearValue=="") {
-        return null;
-    }
-    else if (getDayValue>="32" || getDayValue<="0") {
-        msg.innerText = "Days (1-31)";
+    else if (day >= "32" || day <= "0") {
+        messageLabelEl.innerText = "Days (1-31)";
+        return false;
 
     }
-    else if (getMonthValue>="13" || getMonthValue <="0") {
-        msg.innerText = "Months 1-12";
+    else if (month >= "13" || month <= "0") {
+        messageLabelEl.innerText = "Months 1-12";
+        return false;
 
     }
-    else if (getYearValue>="3001"|| getYearValue<="1899") {
-        msg.innerText = "Years 1900-3000";
-    }
-    else{
-        msg.innerText="";
-        document.getElementById("ageShow").innerHTML = years + " Years "+"<br>"+ months + "  Months " +"<br>"+ days + " Days";
+    else if (year >= "3001" || year <= "1899") {
+        messageLabelEl.innerText = "Years 1900-3000";
+        return false;
     }
 
+    messageLabelEl.innerText = "";
+    return true;
 }
-    
- ) 
- 
 
+
+function calculateBirthDate({ day, month, year }) {
+    const today = new Date();
+    let currDay = today.getDate();
+    let currMonth = 1 + today.getMonth();
+    let currYear = today.getFullYear();
+    const isLeapYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    const monthDays = [31, isLeapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (day > currDay) {
+        currDay += monthDays[currMonth - 1];
+        currMonth--;
+    }
+    if (month > currMonth) {
+        currMonth += 12;
+        currYear--;
+    }
+
+    let days = currDay - day;
+    let months = currMonth - month;
+    let years = currYear - year;
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    return {
+        days,
+        months,
+        years,
+    }
+}
+
+
+
+function getBirthDateValues() {
+    const day = Number(document.getElementById("days").value);
+    const month = Number(document.getElementById("months").value);
+    const year = Number(document.getElementById("years").value);
+
+
+    return { day, month, year };
+}
+
+
+
+age.addEventListener("click", () => {
+    const userBirthDate = getBirthDateValues();
+    console.log (userBirthDate);
+    const isUserBirthDateValid = validateBirthDate(userBirthDate)
+    if (!isUserBirthDateValid) return;
+
+    const calculatedUserAge = calculateBirthDate(userBirthDate);
+
+    document.getElementById("ageShow").innerHTML = calculatedUserAge.years + " Years " + "<br>" + calculatedUserAge.months + "  Months " + "<br>" + calculatedUserAge.days + " Days";
+}
+
+)
 
